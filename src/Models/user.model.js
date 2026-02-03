66e8daa7027ea9b0ac21a49bedc7bf8e1572cc17
Aarya Patil <122004182+Aarya2892@users.noreply.github.com>
2026-02-03 10:40:38 +0000

    Models

---
+++ src/Models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
